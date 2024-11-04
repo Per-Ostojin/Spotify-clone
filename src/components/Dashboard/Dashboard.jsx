e6018225ab45e942baa3343f09c7f7ec2b974df1
Per Ostojin <perostojin@outlook.com>
2024-11-04 20:35:24 +0100

    Styled Dashboard component

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -3,8 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import Home from '../../pages/Home';
 
 const Dashboard = ({ spotifyApi }) => {
-    return <Box>
-        <Box>
+    return ( <Box sx={{ width: '100%', height: '100%', display: 'flex', flexDirection: 'column' }}>
+        <Box sx={{ flex: 1, overflowY: 'auto', display: 'flex'}}>
             <Routes>
                 <Route path='/playlist/:id' element={<div>Playlist</div>} />
                 <Route path='/library' element={<div>Library</div>} />
@@ -12,6 +12,7 @@ const Dashboard = ({ spotifyApi }) => {
             </Routes>
         </Box>
     </Box>
+    )
 };
 
 export default Dashboard;

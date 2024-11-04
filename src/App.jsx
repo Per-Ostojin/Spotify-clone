4b8579bd737b96541f25576d881a8997cf787a96
Per Ostojin <perostojin@outlook.com>
2024-11-04 20:50:32 +0100

    Added utils with accesstoken and uppdated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import { Box } from '@mui/material';
 import Dashboard from './components/Dashboard/Dashboard';
+import  Login  from './pages/Login';
 
 function App({ spotifyApi }) {
 	console.log(spotifyApi);
 	return (
 		<Box className="App">
+			<Login />
 			<Dashboard spotifyApi={spotifyApi} />
 		</Box>
 	);

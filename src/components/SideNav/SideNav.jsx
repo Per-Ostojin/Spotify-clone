df8acbb2617c3296ccaa79805f0fcb03b527b67a
Per Ostojin <perostojin@outlook.com>
2024-11-06 21:16:33 +0100

    Added SideNav component and updated Dashboard

---
+++ src/components/SideNav/SideNav.jsx
@@ -0,0 +1,7 @@
+import { useState, useEffect} from "react";
+import { Box, Divider } from '@mui/material'
+
+const SideNav = ({ SpotifyApi, token}) => {
+    return <Box>SideNav</Box>;
+};
+export default SideNav;

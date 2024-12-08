5f67a99b7db03b69ac039e5c476cb73233ab0a42
Per Ostojin <perostojin@outlook.com>
2024-12-08 15:29:25 +0100

    Fixed imports and updated Playlist state logic

--- src/pages/Playlist.jsx
+++ src/pages/Playlist.jsx
@@ -1,65 +1,74 @@
-import { Box, Typography, Avatar } from "@mui/material";
-import { useEffect, useState } from "react";
-import { useParams } from 'react-router-dom';
+import { Box, Typography, Avatar } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { useParams, useCallback } from 'react-router-dom';
 
 const Playlist = ({ spotifyApi, token }) => {
-    const [playlistInfo, setplaylistInfo] = useState();
-    const { songs, setSongs} = useState();
-    const { id } = useParams();
+	const [playlistInfo, setplaylistInfo] = useState();
+	const { songs, setSongs } = useState();
+	const { id } = useParams();
 
-    const formatSongs = (items) => items.map((item, i) => {
-        console.log({item, i})
-        const {track} = item;
-        track.contextUri = 'spotify:playlist:${id}';
-        track.position = i;
-        return track;
-    });
+	const formatSongs = useCallback(
+		(items) =>
+			items.map((item, i) => {
+				console.log({ item, i });
+				const { track } = item;
+				track.contextUri = 'spotify:playlist:${id}';
+				track.position = i;
+				return track;
+			}),
+		[id]
+	);
 
-    useEffect(() => {
-        const getData = async () => {
-            try {
-                const playlistDetails = await spotifyApi.getPlaylist(id)
-                setplaylistInfo({
-                    image: playlistDetails.body.images[0].url,
-                    name: playlistDetails.body.name
-                })
-                console.log(playlistDetails);
-                const{ items } = playlistDetails.body.tracks;
-                const formatedSong= formatSongs(items);
-                setSongs(formatedSong);
-            } catch(e) {
-                console.error(e);
-            }
-        }
+	useEffect(() => {
+		const getData = async () => {
+			try {
+				const playlistDetails = await spotifyApi.getPlaylist(id);
+				setplaylistInfo({
+					image: playlistDetails.body.images[0].url,
+					name: playlistDetails.body.name
+				});
+				console.log(playlistDetails);
+				const { items } = playlistDetails.body.tracks;
+				const formatedSong = formatSongs(items);
+				setSongs(formatedSong);
+			} catch (e) {
+				console.error(e);
+			}
+		};
 
-        getData();
-    }, [id]);
-    
-    return <Box id="Playlist__page" sx={{backgroundColor: 'background.paper', flex: 1, overflow: 'auto', }} >
-        <Box p={{ xs: 3, md: 4 }} sx={{
-            width: '100%',
-            background: 'linear-gradient(0deg, #121212 0%, #1bd76060 100%);',
-            display: 'flex',
-            justifyContent: 'flex-start',
-            alignItems: {xs: 'flex-start', md: "flex-end", xl: 'center'},
-            gap: 3,
-            boxSizing: "border-box",
-            flexDirection: { xs: 'column', md: 'row' }
+		getData();
+	}, [id, formatSongs]);
 
-        }} >
-            <Avatar 
-            src={playlistInfo?.image} 
-            variant="square" 
-            alt={playlistInfo?.name} 
-            sx={{ boxShadow: 15, width: { xs: '100%', md: 235 }, height: { xs: '100%', md: 235} }} />
-            <Box>
-                <Typography sx={{fontSize: 12, fontWeight: 'bold', color: 'text.primary'}}>Playlist</Typography>
-                <Typography sx={{fontSize: { xs: 42, md: 72 }, fontWeight: 'bold', color: 'text.primary' }}>
-                    {playlistInfo?.name}
-                    </Typography>
-            </Box>
-        </Box>
-    </Box>
+	return (
+		<Box id="Playlist__page" sx={{ backgroundColor: 'background.paper', flex: 1, overflow: 'auto' }}>
+			<Box
+				p={{ xs: 3, md: 4 }}
+				sx={{
+					width: '100%',
+					background: 'linear-gradient(0deg, #121212 0%, #1bd76060 100%);',
+					display: 'flex',
+					justifyContent: 'flex-start',
+					alignItems: { xs: 'flex-start', md: 'flex-end', xl: 'center' },
+					gap: 3,
+					boxSizing: 'border-box',
+					flexDirection: { xs: 'column', md: 'row' }
+				}}
+			>
+				<Avatar
+					src={playlistInfo?.image}
+					variant="square"
+					alt={playlistInfo?.name}
+					sx={{ boxShadow: 15, width: { xs: '100%', md: 235 }, height: { xs: '100%', md: 235 } }}
+				/>
+				<Box>
+					<Typography sx={{ fontSize: 12, fontWeight: 'bold', color: 'text.primary' }}>Playlist</Typography>
+					<Typography sx={{ fontSize: { xs: 42, md: 72 }, fontWeight: 'bold', color: 'text.primary' }}>
+						{playlistInfo?.name}
+					</Typography>
+				</Box>
+			</Box>
+		</Box>
+	);
 };
 
 export default Playlist;

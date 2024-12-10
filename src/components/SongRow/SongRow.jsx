72007b0aa5dfa001333390f28da15077d700023e
Per Ostojin <perostojin@outlook.com>
2024-12-10 20:58:09 +0100

    Enhanced SongRow functionality to play songs and updated SongTable

--- src/components/SongRow/SongRow.jsx
+++ src/components/SongRow/SongRow.jsx
@@ -1,9 +1,17 @@
 import { Avatar, Box, Grid, Typography, Skeleton } from '@mui/material';
 import { formatTime } from '../../utils/formatTime';
-const SongRow = ({ images, title, artist, album, duration, i, loading }) => {
+
+const SongRow = ({ images, title, artist, album, duration, i, loading, position, contextUri, spotifyApi }) => {
 	const image = images?.length > 0 ? images[0].url : null;
+
+	const onRowClick = async () => {
+		const song = { context_uri: contextUri, offset: {position: position}, position_ms: 0 }
+        await spotifyApi.play(song);
+	};
+
 	return (
 		<Grid
+			onClick={onRowClick}
 			container
 			px={2}
 			p={1}
@@ -19,21 +27,25 @@ const SongRow = ({ images, title, artist, album, duration, i, loading }) => {
 				{i + 1}
 			</Grid>
 			<Grid item sx={{ flex: 1, display: 'flex', alignItems: 'center', gap: 2 }}>
-				{loading ? <Skeleton variant='rectangular' width={40} height={40} /> : <Avatar src={image} alt={null} variant="square" /> }
+				{loading ? (
+					<Skeleton variant="rectangular" width={40} height={40} />
+				) : (
+					<Avatar src={image} alt={null} variant="square" />
+				)}
 				<Box>
 					<Typography sx={{ fontSize: 16, color: 'text.primary' }}>
-                        {loading ? <Skeleton variant='text' width={130} height={24} /> : title}
-                    </Typography>
+						{loading ? <Skeleton variant="text" width={130} height={24} /> : title}
+					</Typography>
 					<Typography sx={{ fontSize: 12, color: 'text.secondary' }}>
-                        {loading ? <Skeleton variant='text' width={50} height={18} /> : artist}
-                    </Typography>
+						{loading ? <Skeleton variant="text" width={50} height={18} /> : artist}
+					</Typography>
 				</Box>
 			</Grid>
 			<Grid xs={3} item sx={{ display: { sx: 'none', md: 'flex', alignItems: 'center' } }}>
-            {loading ? <Skeleton variant='text' width={50} height={24} /> : album}
+				{loading ? <Skeleton variant="text" width={50} height={24} /> : album}
 			</Grid>
 			<Grid xs={3} item sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
-            {loading ? <Skeleton variant='text' width={50} height={24} /> : formatTime(duration)}
+				{loading ? <Skeleton variant="text" width={50} height={24} /> : formatTime(duration)}
 			</Grid>
 		</Grid>
 	);

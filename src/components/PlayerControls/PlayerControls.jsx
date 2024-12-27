d6877fdbe0c8c2e44273779adf70cccf66dd4dfb
Per Ostojin <perostojin@outlook.com>
2024-12-27 22:46:34 +0100

    Updated PlayerControls: Improved styling and refactored play/pause logic for better readability

--- src/components/PlayerControls/PlayerControls.jsx
+++ src/components/PlayerControls/PlayerControls.jsx
@@ -1,23 +1,37 @@
-import { Stack, Typography, Slider, Box, IconButton } from "@mui/material";
+import { Stack, Typography, Slider, Box, IconButton } from '@mui/material';
 import { formatTime } from '../../utils/formatTime';
-import { PlayArrow, SkipNext, SkipPrevious, Pause } from "@mui/icons-material";
-import { useState } from "react";
+import { PlayArrow, SkipNext, SkipPrevious, Pause } from '@mui/icons-material';
+import { useState } from 'react';
 
 const PlayerControls = ({ is_paused, duration, progress, player }) => {
-    const skipStyle = { width: 28, height: 28 }
-    const playStyle = { width: 38, height: 38 }
-    return ( <Stack direction={'column'} spacing={2} justifyContent={'center'} alignItems={'center'} sx={{ width: '100%' }} >
-        <Stack spacing={1} direction={'row'} justifyContent={'center'} alignItems={'center'} sx={{ width: '100%' }} >
-            <IconButton size='small' sx={{color: 'text.primary'}} >
-                <SkipPrevious sx={skipStyle} />
-            </IconButton>
-            <IconButton size='small' sx={{color: 'text.primary'}} >{is_paused ? <PlayArrow sx={playStyle} /> : <Pause sx={{playStyle}} /> }</IconButton>
-            <IconButton size='small' sx={{color: 'text.primary'}} >
-                <SkipNext sx={skipStyle} />
-            </IconButton>
-        </Stack>
-        <Stack>Progress || Slider</Stack>
-    </Stack> );
-}
- 
+	const skipStyle = { width: 28, height: 28 };
+	const playStyle = { width: 38, height: 38 };
+	return (
+		<Stack direction={'column'} spacing={2} justifyContent={'center'} alignItems={'center'} sx={{ width: '100%' }}>
+			<Stack spacing={1} direction={'row'} justifyContent={'center'} alignItems={'center'} sx={{ width: '100%' }}>
+				<IconButton size="small" sx={{ color: 'text.primary' }}>
+					<SkipPrevious sx={skipStyle} />
+				</IconButton>
+				<IconButton size="small" sx={{ color: 'text.primary' }}>
+					{is_paused ? <PlayArrow sx={playStyle} /> : <Pause sx={{ playStyle }} />}
+				</IconButton>
+				<IconButton size="small" sx={{ color: 'text.primary' }}>
+					<SkipNext sx={skipStyle} />
+				</IconButton>
+			</Stack>
+			<Stack
+				spacing={2}
+				direction={'row'}
+				justifyContent={'center'}
+				alignItems={'center'}
+				sx={{ width: '75%' }}
+			>
+                <Typography sx={{color: 'text.secondary', fontSize: 12 }} >{formatTime(progress)}</Typography>
+                <Slider max={duration} value={progress} min={0} size='medium' />
+                <Typography sx={{color: 'text.secondary', fontSize: 12 }} >{formatTime(duration)}</Typography>
+            </Stack>
+		</Stack>
+	);
+};
+
 export default PlayerControls;

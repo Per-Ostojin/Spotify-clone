772553848173177c72d20c2d3384ca679e1762a2
Per Ostojin <perostojin@outlook.com>
2024-12-27 22:27:12 +0100

    Enhanced PlayerControls component: Added play/pause and skip button functionality with styling updates

--- src/components/PlayerControls/PlayerControls.jsx
+++ src/components/PlayerControls/PlayerControls.jsx
@@ -4,8 +4,18 @@ import { PlayArrow, SkipNext, SkipPrevious, Pause } from "@mui/icons-material";
 import { useState } from "react";
 
 const PlayerControls = ({ is_paused, duration, progress, player }) => {
-    return ( <Stack direction={'column'} spacing={2} justify={'center'} sx={{ width: '100%' }} >
-        <Stack>Play knappar</Stack>
+    const skipStyle = { width: 28, height: 28 }
+    const playStyle = { width: 38, height: 38 }
+    return ( <Stack direction={'column'} spacing={2} justifyContent={'center'} alignItems={'center'} sx={{ width: '100%' }} >
+        <Stack spacing={1} direction={'row'} justifyContent={'center'} alignItems={'center'} sx={{ width: '100%' }} >
+            <IconButton size='small' sx={{color: 'text.primary'}} >
+                <SkipPrevious sx={skipStyle} />
+            </IconButton>
+            <IconButton size='small' sx={{color: 'text.primary'}} >{is_paused ? <PlayArrow sx={playStyle} /> : <Pause sx={{playStyle}} /> }</IconButton>
+            <IconButton size='small' sx={{color: 'text.primary'}} >
+                <SkipNext sx={skipStyle} />
+            </IconButton>
+        </Stack>
         <Stack>Progress || Slider</Stack>
     </Stack> );
 }

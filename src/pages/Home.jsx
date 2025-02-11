6ed819ff8e57d613b691fb9c62a7f83d0a189a94
Per Ostojin <perostojin@outlook.com>
2025-02-11 10:36:37 +0100

    Updated Home.jsx and added new picture

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import { Box, Button } from '@mui/material'
+import { Box, Button } from '@mui/material';
 
 const Home = () => {
-    return <Box sx={{
-                        flex: 1,
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        flexDirection: 'column',
-                        gap: 5
-    }}>
-        <img src="/TA-logo.png" alt="Techover" style={{ maxWidth: '50%', maxHeight: '50%'}}/>
-        <Button size='large' variant='contained' href='mailto:perostojin@outlook.com'>Kontakta Mig!</Button>
-    </Box>
+	return (
+		<Box
+			sx={{
+				flex: 1,
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				flexDirection: 'column',
+				gap: 5
+			}}
+		>
+			<img src="/perra.png" alt="Per Ostojin" style={{ maxWidth: '50%', maxHeight: '50%' }} />
+			<Button
+				size="large"
+				variant="contained"
+				target="_blank"
+				rel="noopener noreferrer"
+				href="https://pers-social-links.netlify.app/"
+			>
+				Kontakta Mig!
+			</Button>
+		</Box>
+	);
 };
 
 export default Home;
